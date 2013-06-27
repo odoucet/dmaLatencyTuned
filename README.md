14917ded385e4a3963a40a8389c606bae13d6ada
Olivier Doucet <olivier@oxeva.fr>
2013-06-27 19:58:55 +0200

    Commit first version.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 dmaLatencyTuned
 ===============
+Power consumption is based on C-States.
+http://pm-blog.yarda.eu/2011/10/deeper-c-states-and-increased-latency.html
 
-Adjust /dev/cpu_dma_latency based on CPU usage
+You can adjust kernel response time - and accordingly CPU consumption - by writing to /dev/cpu_dma_latency
+
+This tool monitors /proc/stat and write to this file to get the most effective value : 
+if CPU is idle, allow C6/C7, and then change based on load.
+When CPU is heavily used, stay in C0
+
+To see C-States / dma_latency value relationship : 
+cat /sys/devices/system/cpu/cpu0/cpuidle/state*/{name,latency}
---
+++ dmalatency.php.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+<?php
+
+/*****************
+ * DMA Latency Tuned
+ * Adjust /dev/cpu_dma_latency based on CPU load
+ *
+ * @author Olivier Doucet <olivier at oxeva dot fr>
+ ***************/
+
+// Interval chosen to update value (in seconds)
+define('LOOPTIME', 10);
+
+// if CPU > X, then disable all C-States
+$latencyValues = array(
+    // CPU => value   (please order by CPU DESC)
+    50     => 0,  // disable all (full speed)
+    10     => 11, // C1E state
+    3      => 201,// C6  state
+);
+
+$oldValues = array(
+    'busy' => 0,
+    'idle' => 0,
+);
+
+// old that file descriptor 
+$fp = fopen('/dev/cpu_dma_latency', 'w');
+
+while (true) {
+    // Get new values
+    $z = preg_match(
+        '@cpu\s+([0-9]{1,}) ([0-9]{1,}) ([0-9]{1,}) ([0-9]{1,}) ([0-9]{1,})'.
+        ' ([0-9]{1,}) ([0-9]{1,}) @',
+        file_get_contents('/proc/stat'),
+        $data
+    );
+    
+    if (!$z) {
+        // fail ... no infinite loop plz
+        sleep(1);
+        continue;
+    }
+    
+    $newValues = array(
+        'busy' => $data[1]+$data[2]+$data[3]+$data[5]+$data[6]+$data[7],
+        'idle' => $data[4],
+    );
+    if ($oldValues['busy'] == 0) {
+        // Wait
+        $oldValues = $newValues;
+        sleep(LOOPTIME);
+        continue;
+    }
+    
+    $busyPercent = ($newValues['busy'] - $oldValues['busy'])/
+                   ($newValues['busy'] - $oldValues['busy']+$newValues['idle'] - $oldValues['idle'])
+                   *100;
+    printf("CPU Usage: %5.2f %%  \r", $busyPercent);
+    
+    // Do the stuff
+    foreach ($latencyValues as $cpuUsage => $latencyNeeded) {
+        if ($busyPercent >= $cpuUsage)
+            break;
+    }   
+    
+    fwrite($fp, pack('i', $latencyNeeded));
+    rewind($fp);
+    
+    $oldValues = $newValues;
+    sleep(LOOPTIME);
+}
+fclose($fp);
